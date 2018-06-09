6ec5fcaf14f40ec99f2ee61921e39cf0524bbe61
Mathias Quintero <me@quintero.io>
2018-06-09 11:21:01 +0200

    Fix it

--- js/StoriesCtrl.js
+++ js/StoriesCtrl.js
@@ -29,8 +29,8 @@ app.controller('StoriesCtrl', function($scope, $routeParams, $http) {
     function loadStories(callback) {
         loadPostings((postings) => {
             if (postings !== null) callback(null);
-            var withMedia = postings.filter((x) => x.media);
-            var byTeams = postings.bulked((x) => x.user.participant.teamId && x.user.profilePic);
+            var withMedia = postings.filter((x) => x.media !== null && x.user.profilePic !== null);
+            var byTeams = postings.bulked((x) => x.user.participant.teamId );
             var stories = byTeams.map((x) => {
                 var user = x[0].user;
                 var posts = x;
